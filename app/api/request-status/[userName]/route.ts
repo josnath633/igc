31fa89a3ce2ab6ca7554e038f43f3531334d5665
unknown <elieboulingui2@gmail.com>
2025-05-16 15:54:31 +0100

    update retake last version

--- app/api/request-status/[userName]/route.ts
+++ app/api/request-status/[userName]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma"; // Ensure Prisma is correctly set up
+import {prisma} from "@/lib/prisma"; // Ensure Prisma is correctly set up
 
 export async function GET(req: NextRequest) {
   try {
